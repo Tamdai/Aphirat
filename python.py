db049f9f583903608aa4d4adfce35b645b759407
Aphirat <ben.530210155@gmail.com>
2024-12-14 10:53:47 +0700

    first commit

---
+++ python.py
@@ -0,0 +1,5 @@
+from roboflow import Roboflow
+rf = Roboflow(api_key="fF8fftBxEHKCmZJYvSeZ")
+project = rf.workspace("rf-projects").project("x-ray-id")
+version = project.version(3)
+dataset = version.download("yolov11")
