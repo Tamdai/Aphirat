46bd5687f7fd8d96a6a8db4532943d5c8ef6810b
Ben_Aphirat <ben.530210155@gmail.com>
2024-12-16 12:37:52 +0700

    0

--- python.py
+++ python.py
@@ -3,5 +3,3 @@ rf = Roboflow(api_key="fF8fftBxEHKCmZJYvSeZ")
 project = rf.workspace("rf-projects").project("x-ray-id")
 version = project.version(3)
 dataset = version.download("yolov11")
-..
-123456789
